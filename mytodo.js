3095ff2bea4b783562fa075338120f525cc75943
Pritika Bansal <117353768+Pritikabansal@users.noreply.github.com>
2024-07-11 04:26:32 +0530

    Add files via upload

---
+++ mytodo.js
@@ -0,0 +1,41 @@
+let btn = document.querySelector('button');
+let ul = document.querySelector('ul');
+let inp = document.querySelector('input');
+
+
+
+btn.addEventListener('click', function () {
+
+    let item = document.createElement('li');
+
+    let checkBtn = document.createElement('input');
+    checkBtn.type = "checkbox";
+    checkBtn.className = "checkk";
+    checkBtn.title = "Checkbox";
+
+    let itemText = document.createTextNode(inp.value);
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.className = 'delete';
+    deleteBtn.title = "Delete task";
+
+    let iconn = document.createElement('i');
+    iconn.className = "fa-solid fa-xmark";
+
+
+    deleteBtn.appendChild(iconn);
+    item.appendChild(checkBtn);
+    item.appendChild(itemText);
+    item.appendChild(deleteBtn);
+
+    ul.appendChild(item);
+    inp.value = "";
+});
+
+ul.addEventListener('click', function (event) {
+    if (event.target.classList.contains('fa-xmark') || event.target.nodeName === "BUTTON") {
+        let lItem = event.target.closest('li');
+        lItem.remove();
+    }
+});
+
